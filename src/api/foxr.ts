80e880332de7720706a29eafeba8db74b940c44a
Kir Belevich <kir@belevi.ch>
2018-08-09 19:40:12 +0200

    🛠 move everything about Socket from api/foxr to Protocol (close #7)

--- src/api/foxr.ts
+++ src/api/foxr.ts
@@ -1,77 +1,26 @@
-import { Socket } from 'net'
-
-import { parse } from '../transport'
-import { handleStream } from '../protocol'
+import connectToMarionette from '../protocol'
 import { createBrowser } from './browser'
 
 const DEFAULT_HOST = 'localhost'
 const DEFAULT_PORT = 2828
-const CONNECTION_TIMEOUT = 10000
 
-type TOptions = {
+type TConnectOptions = {
   host?: string,
   port?: number
 }
 
-class TFoxrError extends Error {
-  constructor (message: string) {
-    super(message)
-    Error.captureStackTrace(this, TFoxrError)
-    this.name = 'FoxrError'
-  }
-}
-
 const foxr = {
-  connect: async (options?: TOptions) => {
-    try {
-      const { host, port } = {
-        ...options,
-        host: DEFAULT_HOST,
-        port: DEFAULT_PORT
-      }
-      const socket = new Socket()
-
-      await new Promise<void>((resolve, reject) => {
-        const rejectAndDestroy = (error: Error) => {
-          reject(error)
-          socket.destroy()
-        }
-
-        socket
-          .setTimeout(CONNECTION_TIMEOUT)
-          .once('ready', () => {
-            socket.once('data', (rawData) => {
-              const data = parse(rawData)
-
-              if (data.applicationType === 'gecko') {
-                if (data.marionetteProtocol === 3) {
-                  return resolve()
-                }
-
-                return rejectAndDestroy(new Error('Foxr works only with Marionette protocol v3'))
-              }
-
-              rejectAndDestroy(new Error('Marionette connection protocol error'))
-            })
-          })
-          .once('timeout', () => rejectAndDestroy(new Error('Socket connection timeout')))
-          .once('error', rejectAndDestroy)
-          .connect(port, host)
-      })
-
-      const send = handleStream(socket)
-
-      // TODO: is the result needed?
-      await send('WebDriver:NewSession', { capabilities: {} })
-
-      return createBrowser(send, () => {
-        socket.end()
-      })
-    } catch (err) {
-      throw new TFoxrError(
-        err.stack.replace(/^Error:\s/, '').replace(/\s\s\s\s\s/, '\n')
-      )
+  connect: async (options?: TConnectOptions) => {
+    const { host, port } = {
+      ...options,
+      host: DEFAULT_HOST,
+      port: DEFAULT_PORT
     }
+    const { send, disconnect } = await connectToMarionette(host, port)
+
+    await send('WebDriver:NewSession', { capabilities: {} })
+
+    return createBrowser(send, disconnect)
   }
 }
 
--- src/protocol.ts
+++ src/protocol.ts
@@ -1,12 +1,13 @@
 import { TJsonMap, TJsonValue } from 'typeon'
+import { Socket } from 'net'
 
-import { createParseStream, stringify } from './transport'
+import { createParseStream, parse, stringify } from './transport'
 
-type TResult = [number, number, null | Error, TJsonValue]
+const CONNECTION_TIMEOUT = 10000
 
 export type TSend = (name: string, params?: TJsonMap) => Promise<any>
 
-export const handleStream = (socket: NodeJS.Socket): TSend => {
+const connectToMarionette = async (host: string, port: number) => {
   type TQueueItem = {
     id: number,
     resolve: (arg: any) => void,
@@ -14,9 +15,39 @@ export const handleStream = (socket: NodeJS.Socket): TSend => {
   }
   let globalId = 0
   let queue: TQueueItem[] = []
+  const socket = new Socket()
+
+  await new Promise<void>((resolve, reject) => {
+    const rejectAndDestroy = (error: Error) => {
+      reject(error)
+      socket.destroy()
+    }
+
+    socket
+      .setTimeout(CONNECTION_TIMEOUT)
+      .once('ready', () => {
+        socket.once('data', (rawData) => {
+          const data = parse(rawData)
+
+          if (data.applicationType === 'gecko') {
+            if (data.marionetteProtocol === 3) {
+              return resolve()
+            }
+
+            return rejectAndDestroy(new Error('Foxr works only with Marionette protocol v3'))
+          }
+
+          rejectAndDestroy(new Error('Marionette connection protocol error'))
+        })
+      })
+      .once('timeout', () => rejectAndDestroy(new Error('Socket connection timeout')))
+      .once('error', rejectAndDestroy)
+      .connect(port, host)
+  })
+
   const parseStream = createParseStream()
 
-  parseStream.on('data', (data: TResult) => {
+  parseStream.on('data', (data: [number, number, null | Error, TJsonValue]) => {
     const [type, id, error, result] = data
 
     if (type === 1) {
@@ -38,14 +69,22 @@ export const handleStream = (socket: NodeJS.Socket): TSend => {
 
   socket.pipe(parseStream)
 
-  return (name, params = {}) => {
-    return new Promise((resolve, reject) => {
-      const data: string = stringify([0, globalId, name, params])
+  return {
+    disconnect: (): void => {
+      socket.end()
+    },
 
-      socket.write(data, 'utf8', () => {
-        queue.push({ id: globalId, resolve, reject })
-        globalId += 1
+    send: (name: string, params: TJsonMap = {}): Promise<any> => {
+      return new Promise((resolve, reject) => {
+        const data: string = stringify([0, globalId, name, params])
+
+        socket.write(data, 'utf8', () => {
+          queue.push({ id: globalId, resolve, reject })
+          globalId += 1
+        })
       })
-    })
+    }
   }
 }
+
+export default connectToMarionette
