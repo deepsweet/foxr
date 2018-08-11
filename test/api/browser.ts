7511bb81ca6bf5208634bbdfc2157c2839f85ccf
Kir Belevich <kir@belevi.ch>
2018-08-11 18:39:32 +0200

    👾 browser: test `close()` + `disconnected` event

--- test/api/browser.ts
+++ test/api/browser.ts
@@ -1,10 +1,29 @@
 import test from 'blue-tape'
 import createBrowser from '../../src/api/browser'
+import foxr from '../../src/api/foxr'
+import { testWithFirefox } from '../helpers/firefox'
+import { createSpy, getSpyCalls } from 'spyfn'
 
-test('browser: createBrowser', (t) => {
+test('createBrowser', (t) => {
   t.true(
     typeof createBrowser === 'function',
     'is a function'
   )
   t.end()
 })
+
+test('browser: close + `disconnected` event', testWithFirefox(async (t) => {
+  const browser = await foxr.connect()
+  const onDisconnectSpy = createSpy(() => {})
+
+  browser.once('disconnected', onDisconnectSpy)
+
+  // TODO: figure out how to test this for real
+  await browser.close()
+
+  t.deepEqual(
+    getSpyCalls(onDisconnectSpy),
+    [[]],
+    'should emit `disconnected` event'
+  )
+}))
