c846f7bb3cf83537b589b651f76414ac8ed82d43
Kir Belevich <kir@belevi.ch>
2018-08-11 18:57:44 +0200

    👾 foxr: add TODO

--- test/api/foxr.ts
+++ test/api/foxr.ts
@@ -2,8 +2,9 @@ import test from 'blue-tape'
 import foxr from '../../src/api/foxr'
 import { testWithFirefox } from '../helpers/firefox'
 
-test('foxr: connect', testWithFirefox(async (t) => {
+test('foxr: `connect()`', testWithFirefox(async (t) => {
   const browser = await foxr.connect()
 
+  // TODO: check `browser` for real
   t.ok(browser, 'should connect')
 }))
