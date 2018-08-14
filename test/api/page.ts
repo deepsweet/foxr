458f1d04f05470c132d034a356e6efa62e039fc1
Kir Belevich <kir@belevi.ch>
2018-08-14 16:59:37 +0200

    👾 page: add tests for `content()` and `setContent()`

--- test/api/page.ts
+++ test/api/page.ts
@@ -1,10 +1,32 @@
 import test from 'blue-tape'
 import createPage from '../../src/api/page'
+import foxr from '../../src/api/foxr'
+import { testWithFirefox } from '../helpers/firefox'
 
-test('page: createPage', (t) => {
+test('createPage', (t) => {
   t.true(
     typeof createPage === 'function',
     'is a function'
   )
   t.end()
 })
+
+test('page: `setContent` + `content`', testWithFirefox(async (t) => {
+  const browser = await foxr.connect()
+  const page = await browser.newPage()
+  const html = '<h1>hello</h1>'
+
+  t.equal(
+    await page.content(),
+    '<html><head></head><body></body></html>',
+    'content() should return page HTML'
+  )
+
+  await page.setContent(html)
+
+  t.equal(
+    await page.content(),
+    `<html><head></head><body>${html}</body></html>`,
+    'setContent() should set page HTML'
+  )
+}))
