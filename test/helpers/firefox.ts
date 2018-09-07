d334b5c4c52b7284a127d05dd33d3ad548b36a95
Kir Belevich <kir@belevi.ch>
2018-09-07 19:27:17 +0200

    🛠 test with Firefox 62

--- test/helpers/firefox.ts
+++ test/helpers/firefox.ts
@@ -3,7 +3,7 @@ import { Test } from 'blue-tape'
 import waitForMarionette from './wait-for-marionette'
 
 export const runFirefox = () => execa('docker',
-  'run -id --rm --shm-size 2g -p 2828:2828 --name foxr-firefox deepsweet/firefox-headless-remote:61'.split(' ')
+  'run -id --rm --shm-size 2g -p 2828:2828 --name foxr-firefox deepsweet/firefox-headless-remote:62'.split(' ')
 )
 
 export const stopFirefox = () => execa('docker',
