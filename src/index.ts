60da156733488aa95a57bb3fd6a9ef9244a013f9
Kir Belevich <kir.belevich@klarna.com>
2018-11-09 16:59:56 +0100

    ✔️ export API types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import Foxr from './api/Foxr'
+import Browser from './api/Browser'
+import Page from './api/Page'
+import JSHandle from './api/JSHandle'
+import ElementHandle from './api/ElementHandle'
 
 export default new Foxr()
+
+export type TBrowser = Browser
+export type TPage = Page
+export type TJSHandle = JSHandle
+export type TElementHandle = ElementHandle
